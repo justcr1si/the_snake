ae13686357c89def76f6aad86221c1c033ab1205
Daniil Moskalenko <justcr1si@yandex.ru>
2024-07-19 00:58:08 +0300

    Reset game to classic game for fourth review

--- the_snake.py
+++ the_snake.py
@@ -26,12 +26,6 @@ APPLE_COLOR = (255, 0, 0)
 # Цвет змейки
 SNAKE_COLOR = (0, 255, 0)
 
-# Цвет отравленной еды
-POISONED_FOOD_COLOR = (100, 40, 0)
-
-# Цвет камня
-STONE_COLOR = (235, 245, 255)
-
 # Скорость движения змейки
 SPEED = 20
 
@@ -61,17 +55,21 @@ class GameObject:
         """Абстрактный метод, который рисует объект на игровом поле."""
         pass
 
-    def paint_over(self, position):
-        """Закрашивает клетку на определенной позиции."""
+    def draw_cell(self, position, paint_over=False):
+        """Рисует клетку на определенной позиции."""
         rect = pygame.Rect(position,
                            (GRID_SIZE, GRID_SIZE))
-        pygame.draw.rect(screen, BOARD_BACKGROUND_COLOR, rect)
+        if paint_over:
+            pygame.draw.rect(screen, BOARD_BACKGROUND_COLOR, rect)
+        else:
+            pygame.draw.rect(screen, self.body_color, rect)
+            pygame.draw.rect(screen, BORDER_COLOR, rect, 1)
 
 
 class EatableObject(GameObject):
     """Описывает абстракцию еды на поле."""
 
-    def __init__(self, body_color=APPLE_COLOR) -> None:
+    def __init__(self, body_color) -> None:
         """Инициализирует объект еды."""
         super().__init__(body_color=body_color)
 
@@ -81,35 +79,12 @@ class EatableObject(GameObject):
                 randrange(0, GRID_HEIGHT * GRID_SIZE,
                           GRID_SIZE))
 
-    def randomize_position(self, positions):
+    def randomize_position(self, snake_positions):
         """Устанавливает объекту случайную позицию."""
-        all_positions = positions.copy()
-        all_positions.pop(self.get_name())
-        random_coordinate = self.generate_random_position()
-        position_x, position_y = random_coordinate
-        while ((position_x, position_y) in positions['snake']
-               or (position_x, position_y) in all_positions.values()):
-            position_x, position_y = self.generate_random_position()
-        self.position = (position_x, position_y)
-        positions[self.get_name()] = self.position
-
-    def draw(self):
-        """
-        Рисует клетку (метод родителя, принцип один и тот же во всех классах
-        потомках, без переопределения в наследниках).
-        """
-        head_rect = pygame.Rect(self.position,
-                                (GRID_SIZE, GRID_SIZE))
-        pygame.draw.rect(screen, self.body_color, head_rect)
-        pygame.draw.rect(screen, BORDER_COLOR, head_rect, 1)
-
-
-class PoisonedFood(EatableObject):
-    """Описывает ядовитую еду."""
-
-    def get_name(self):
-        """Возвращает название объекта."""
-        return 'poisoned_food'
+        new_position = self.generate_random_position()
+        while new_position in snake_positions:
+            new_position = self.generate_random_position()
+        self.position = new_position
 
 
 class Apple(EatableObject):
@@ -118,17 +93,12 @@ class Apple(EatableObject):
     который наследуется от GameObject.
     """
 
-    def get_name(self):
-        """Возвращает название объекта."""
-        return 'apple'
+    def __init__(self, body_color=APPLE_COLOR) -> None:
+        super().__init__(body_color)
 
-
-class Stone(EatableObject):
-    """Описывает абстракцию игрового камня."""
-
-    def get_name(self):
-        """Возвращает название объекта."""
-        return 'stone'
+    def draw(self, paint_over=False):
+        """Рисует клетку."""
+        self.draw_cell(self.position, paint_over=paint_over)
 
 
 class Snake(GameObject):
@@ -139,11 +109,11 @@ class Snake(GameObject):
         super().__init__(body_color=SNAKE_COLOR)
         self.set_default_attributes()
 
-    def set_default_attributes(self):
+    def set_default_attributes(self, direction=RIGHT):
         """Устанавливает начальные атрибуты змейке."""
         self.positions = [self.position]
         self.length = 1
-        self.direction = RIGHT
+        self.direction = direction
         self.last = None
 
     def update_direction(self, direction):
@@ -153,27 +123,24 @@ class Snake(GameObject):
     def move(self):
         """Двигает змейку относительно направления движения."""
         head_coordinates = self.get_head_position()
-        step = [x * GRID_SIZE for x in self.direction]
-        step_x, step_y = step
-        position_x, position_y = head_coordinates[0], head_coordinates[1]
-        new_head_position = ((position_x + step_x) % SCREEN_WIDTH,
-                             (position_y + step_y) % SCREEN_HEIGHT)
+        position_x, position_y = head_coordinates
+        new_head_position = ((position_x + (self.direction[0] * GRID_SIZE))
+                             % SCREEN_WIDTH,
+                             (position_y + (self.direction[1] * GRID_SIZE))
+                             % SCREEN_HEIGHT)
         self.positions.insert(0, new_head_position)
 
-    def draw(self):
-        """Отрисовывает змейку на игровом поле."""
         if len(self.positions) > self.length:
             self.last = self.positions.pop()
         else:
             self.last = None
 
-        head_rect = pygame.Rect(self.get_head_position(),
-                                (GRID_SIZE, GRID_SIZE))
-        pygame.draw.rect(screen, self.body_color, head_rect)
-        pygame.draw.rect(screen, BORDER_COLOR, head_rect, 1)
+    def draw(self):
+        """Отрисовывает змейку на игровом поле."""
+        self.draw_cell(self.get_head_position())
 
         if self.last:
-            self.paint_over(self.last)
+            self.draw_cell(self.last, paint_over=True)
 
     def get_head_position(self):
         """Возвращает координаты головы змейки."""
@@ -181,12 +148,8 @@ class Snake(GameObject):
 
     def reset(self):
         """Сбрасывает всю змейку и ставит ей начальные координаты."""
-        for position in self.positions:
-            rect = pygame.Rect(position,
-                               (GRID_SIZE, GRID_SIZE))
-            pygame.draw.rect(screen, BOARD_BACKGROUND_COLOR, rect)
-        self.set_default_attributes()
-        self.direction = choice([RIGHT, LEFT, UP, DOWN])
+        new_direction = choice([RIGHT, LEFT, UP, DOWN])
+        self.set_default_attributes(direction=new_direction)
 
 
 def handle_keys(game_object):
@@ -211,28 +174,10 @@ def main():
     pygame.init()
     pygame.display.update()
 
-    """
-    Локальная переменная для хранения позиций объектов для
-    дальнейшего сравнения в randomize_position().
-    """
-    positions = {
-
-    }
-
-    # Передаем в инициализатор позиции объектов для дальнейшего сравнивания
     snake = Snake()
-    apple = Apple()  # Передается локальная переменная positions
-    poisoned_food = PoisonedFood(POISONED_FOOD_COLOR)
-    stone = Stone(STONE_COLOR)  # Необходимый первый аргумент
+    apple = Apple()
 
-    positions['snake'] = snake.positions
-    positions['apple'] = apple.position
-    positions['stone'] = stone.position
-    positions['poisoned_food'] = poisoned_food.position
-
-    apple.randomize_position(positions)
-    stone.randomize_position(positions)
-    poisoned_food.randomize_position(positions)
+    apple.randomize_position(snake.positions)
 
     while True:
         clock.tick(SPEED)
@@ -241,39 +186,18 @@ def main():
         snake.move()
         snake.draw()
         apple.draw()
-        poisoned_food.draw()
-        stone.draw()
         snake_head = snake.get_head_position()
 
-        positions['snake'] = snake.positions
-
         if snake_head == apple.position:
-            apple.randomize_position(positions)
             snake.length += 1
             snake.draw()
-        elif snake_head == poisoned_food.position:
-            if snake.length == 1:
-                reset_objects(apple, stone, snake=snake)
-            else:
-                snake.length -= 1
-                snake.draw()
-            poisoned_food.randomize_position(positions)
-        elif snake_head == stone.position:
-            reset_objects(positions, apple, stone, poisoned_food, snake=snake)
+            apple.randomize_position(snake.positions)
         elif snake_head in snake.positions[1:]:
-            reset_objects(positions, apple, stone, poisoned_food, snake=snake)
+            snake.reset()
+            apple.randomize_position()
+            screen.fill(BOARD_BACKGROUND_COLOR)
+
         pygame.display.update()
-
-
-def reset_objects(positions, *args, snake=None):
-    """Сбрасывает объекты."""
-    if snake:
-        snake.reset()
-        # Сбрасываем змейку, иначе ничего не делаем
-        positions['snake'] = snake.positions
-    for object in args:
-        object.paint_over(object.position)
-        object.randomize_position(positions)
 
 
 if __name__ == "__main__":
